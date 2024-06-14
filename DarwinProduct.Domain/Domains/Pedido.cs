af3336da37e88f4c48fc29f10e88a6b5d960eb87
Posccis <lkzq33@gmail.com>
2024-06-14 13:06:51 -0300

    add(main) = Criação e conclusão da camada de dominio

---
+++ DarwinProduct.Domain/Domains/Pedido.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarwinProduct.Domain.Domains
+{
+    public class Pedido
+    {
+        /// <summary>
+        /// Indentificador do pedido
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Nome do cliente
+        /// </summary>
+        public string Cliente { get; set; }
+        /// <summary>
+        /// DAta do pedido
+        /// </summary>
+        public DateTime Data { get; set; }
+        /// <summary>
+        /// Valor Total do pedido
+        /// </summary>
+        public decimal Valor { get; set; }
+        /// <summary>
+        /// Lista de Itens do Pedido
+        /// </summary>
+        IEnumerable<ItemPedido> Items { get; set; }
+    }
+}
---
+++ DarwinProduct.Domain/Interfaces/IItemPedido.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarwinProduct.Domain.Interfaces
+{
+    internal interface IItemPedido
+    {
+        /// <summary>
+        /// Identificador do Item
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Identificador do Pedido
+        /// </summary>
+        public int PedidoId { get; set; }
+        /// <summary>
+        /// Nome do produto
+        /// </summary>
+        public string Produto { get; set; }
+        /// <summary>
+        /// Quantidade desse produto
+        /// </summary>
+        public int QuantidadeDeProduto { get; set; }
+        /// <summary>
+        /// Produto
+        /// </summary>
+        public decimal Preco { get; set; }
+    }
+}
