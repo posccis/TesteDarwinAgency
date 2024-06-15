1a1f69a9d4b5e618aba88ba86bcd995c331fa1d8
Posccis <lkzq33@gmail.com>
2024-06-15 11:47:05 -0300

    add(main) = Criação das classes de profile

---
+++ DarwinProduct.Domain/Profiles/PedidoProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using DarwinProduct.Domain.Domains;
+using DarwinProduct.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarwinProduct.Domain.Profiles
+{
+    public class PedidoProfile : Profile
+    {
+        public PedidoProfile()
+        {
+            CreateMap<PedidoDTORequest, Pedido>();
+            CreateMap<Pedido, PedidoDTOResponse>();
+
+            CreateMap<ItemPedidoDTORequest, ItemPedido>();
+        }
+    }
+}
