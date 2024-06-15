3022ca119353e53d3250dd15617b6162917f1b1b
Posccis <lkzq33@gmail.com>
2024-06-15 00:24:51 -0300

    update(main) = desenvolvimento da controller de pedidos

---
+++ DarwinProduct.API/Controllers/PedidoController.cs
@@ -0,0 +1,148 @@
+﻿using DarwinProduct.Application.Interfaces;
+using DarwinProduct.Domain.Domains;
+using DarwinProduct.Domain.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DarwinProduct.API.Controllers
+{
+    [Route("api/pedidos")]
+    [ApiController]
+    public class PedidoController : ControllerBase
+    {
+        private readonly IPedidoService<Pedido> _pedidoService;
+        private readonly IItemPedidoService<ItemPedido> _itemPedidoService;
+
+        public PedidoController(IPedidoService<Pedido> pedidoService, IItemPedidoService<ItemPedido> itemPedidoService)
+        {
+            _itemPedidoService = itemPedidoService;
+            _pedidoService = pedidoService;
+        }
+
+        /// <summary>
+        /// Endpoint para adicioanr um novo pedido.
+        /// </summary>
+        /// <param name="pedido"></param>
+        /// <returns>Retorna um código http informando se foi inserido junto do objeto.</returns>
+        [HttpPost]
+        public async Task<IActionResult> AdicionaPedido(Pedido pedido) 
+        {
+            try
+            {
+                await _pedidoService.InserirPedido(pedido);
+                return CreatedAtAction(nameof(ObtemPedidoPorId), new { id = pedido.Id }, pedido);
+            }
+            catch (ServiceException sExc)
+            {
+
+                return BadRequest(sExc.Message);
+            }
+            catch (Exception exc)
+            {
+
+                return BadRequest(exc.Message);
+            }
+        }
+        
+        /// <summary>
+        /// Endpoint para atualizar um pedido.
+        /// </summary>
+        /// <param name="pedido"></param>
+        /// <returns>Retorna um código http informando se foi atualizado junto do objeto atualizado.</returns>
+        [HttpPut]
+        public async Task<IActionResult> AtualizaPedido(Pedido pedido) 
+        {
+            try
+            {
+                await _pedidoService.AtualizarPedido(pedido);
+                return CreatedAtAction(nameof(ObtemPedidoPorId), new { id = pedido.Id }, pedido);
+            }
+            catch (ServiceException sExc)
+            {
+
+                return BadRequest(sExc.Message);
+            }
+            catch (Exception exc)
+            {
+
+                return BadRequest(exc.Message);
+            }
+        }
+
+
+
+        /// <summary>
+        /// Endpoint para obter todos os pedidos com seus itens.
+        /// </summary>
+        /// <returns>Lista com todos os pedidos</returns>
+        [HttpGet]
+        public async Task<ActionResult<List<Pedido>>> ObtemTodosOsPedidos()
+        {
+            try
+            {
+                var pedidos = await _pedidoService.ObterTodosPedidos();
+                return Ok(pedidos);
+            }
+            catch (ServiceException sExc)
+            {
+
+                return NotFound(sExc.Message);
+            }
+            catch (Exception exc)
+            {
+
+                return BadRequest(exc.Message);
+            }
+        }
+
+        /// <summary>
+        /// Endpoint para obter o pedido através do seu Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Um objeto do tipo pedido com todos os atributos incluido a lista de itens.</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pedido>> ObtemPedidoPorId(int id) 
+        {
+            try
+            {
+                var pedido = await _pedidoService.ObterPedidoPorId(id);
+                if (pedido is null) throw new ServiceException($"Não foi possivel encontrar nenhum pedido com o id {id}.");
+                return Ok(pedido);
+            }
+            catch (ServiceException sExc)
+            {
+
+                return NotFound(sExc.Message);
+            }
+            catch (Exception exc)
+            {
+
+                return BadRequest(exc.Message);
+            }
+        }
+        /// <summary>
+        /// Endpoint para remover o pedido através do seu Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Um código http informando se a operação foi realizada com sucesso.</returns>
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Pedido>> RemovePedido(int id) 
+        {
+            try
+            {
+                await _pedidoService.RemoverPedido(id);
+                return Ok();
+            }
+            catch (ServiceException sExc)
+            {
+
+                return NotFound(sExc.Message);
+            }
+            catch (Exception exc)
+            {
+
+                return BadRequest(exc.Message);
+            }
+        }
+    }
+}
--- DarwinProduct.API/Program.cs
+++ DarwinProduct.API/Program.cs
@@ -1,11 +1,36 @@
+using DarwinProduct.Application.Interfaces;
+using DarwinProduct.Application.Services;
+using DarwinProduct.Domain.Domains;
+using DarwinProduct.Persistence;
+using DarwinProduct.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+builder.Services.AddDbContext<DarwinProductContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("DarwinProduct.API"));
+});
+
+builder.Services.AddScoped<IPedidoService<Pedido>, PedidoService>();
+builder.Services.AddScoped<IItemPedidoService<ItemPedido>, ItemPedidoService>();
 
 var app = builder.Build();
 
@@ -13,7 +38,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
@@ -21,5 +49,21 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DarwinProductContext>();
+    context.Database.Migrate();
+    await Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+    throw ex;
+    //var logger = services.GetRequiredService<ILogger<Program>>();
+    //logger.LogError(ex, "Ocorreu um erro durante a migration.");
+}
 
 app.Run();
